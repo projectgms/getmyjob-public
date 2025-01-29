0f088796d42553b83930ab68fb385816fbaa74ff
projectgms <github.gms8087@gmail.com>
2025-01-29 10:11:09 +0530

    updated

--- src/pages/authentication/jobseeker/JobseekerLogin.js
+++ src/pages/authentication/jobseeker/JobseekerLogin.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const JobseekerLogin = () => {
   return (
-    <div><h1>jobseekerlogin</h1></div>
+    <div><h1 className='font-semibold'>jobseekerlogin</h1></div>
   )
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [], // Keep this empty unless adding official Tailwind plugins
+};
